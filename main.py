62ad3a8eb8ae797f0487192e20e0c2a09c3684a3
Otniel <50637920+Otnielush@users.noreply.github.com>
2021-04-12 10:16:43 +0300

    structure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from yt_search_l1 import search_1
 from filter_1 import filter_1
 from parser_l3 import you_parse
+from filter_2 import filter_2
 
 
 MAX_VIDEOS_TO_PARSE = 10
@@ -18,6 +19,9 @@ sorted_youtube_links = filter_1(search_sentence, search_results)
 
 #step 3
 parsed_array = you_parse(sorted_youtube_links, MAX_VIDEOS_TO_PARSE)
+
+#step 4
+predictions = filter_2(search_sentence, parsed_array)
 
 
 
