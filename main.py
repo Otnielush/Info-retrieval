089fa7484e6052cba4561b70aa60ab3ae340fa68
Otniel <50637920+Otnielush@users.noreply.github.com>
2021-04-13 01:11:45 +0300

    search debug, views = 0

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+#!/usr/bin/python
+
 from yt_search_l1 import search_1
 from filter_1 import filter_1
 from parser_l3 import you_parse
 from filter_2 import filter_2
 
+import sys
+
 
 MAX_VIDEOS_TO_PARSE = 10
 
-
-#TODO from console read sentence to search
-search_sentence = 'growing forest'
-
+#read sentence to search from console
+if len(sys.argv) < 2:
+    print('Please write a search words like: "python main.py search words"')
+    exit()
+else:
+    search_sentence = ' '.join(sys.argv[1:])
+print('Searching:', search_sentence)
 # step 1
 search_results = search_1(search_sentence)
 print('Videos found:', len(search_results))
--- yt_search_l1.py
+++ yt_search_l1.py
@@ -14,14 +14,23 @@ def search_1(sentence, num_of_results=50):
     for i in range(num_of_results//20 + 1):
         page = videosSearch.result()['result']
         for p in page:
-            if len(p['duration'].split(':')) > 2:
-                date_time = datetime.datetime.strptime(p['duration'], '%H:%M:%S')
+            if p['duration']:
+                if len(p['duration'].split(':')) > 2:
+                    date_time = datetime.datetime.strptime(p['duration'], '%H:%M:%S')
+                else:
+                    date_time = datetime.datetime.strptime(p['duration'], '%M:%S')
+                a_timedelta = date_time - datetime.datetime(1900, 1, 1)
+                seconds = a_timedelta.total_seconds()
+                p['duration'] = int(seconds)
             else:
-                date_time = datetime.datetime.strptime(p['duration'], '%M:%S')
-            a_timedelta = date_time - datetime.datetime(1900, 1, 1)
-            seconds = a_timedelta.total_seconds()
-            p['duration'] = int(seconds)
-            p['viewCount']['text'] = p['viewCount']['text'][:-6].replace(',', '')
+                p['duration'] = 0
+
+            if p['viewCount']['text']:
+                p['viewCount']['text'] = int(p['viewCount']['text'][:-6].replace(',', ''))
+            else:
+                p['viewCount']['text'] = 0
+
+        # TODO if views and duration = 0 it`s most likely life stream and we don`t need it
         array_results.extend(page)
 
         videosSearch.next()
